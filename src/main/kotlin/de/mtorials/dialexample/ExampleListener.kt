041bf6ae3aaf7697324ab19e8869ee7e800c80cb
mtorials <matti.bleckmann@live.com>
2020-06-15 22:18:54 +0200

    Updated Example

---
+++ src/main/kotlin/de/mtorials/dialexample/ExampleListener.kt
@@ -0,0 +1,12 @@
+package de.mtorials.dialexample
+
+import de.mtorials.dial.events.MessageReceivedEvent
+import de.mtorials.dial.listener.ListenerAdapter
+
+class ExampleListener: ListenerAdapter() {
+    override suspend fun onRoomMessageReceive(event: MessageReceivedEvent) {
+        // Don't answer to your own messages
+        if (event.senderId == event.phone.ownId) return
+        println("user ${event.senderId} send ${event.content.body}")
+    }
+}
