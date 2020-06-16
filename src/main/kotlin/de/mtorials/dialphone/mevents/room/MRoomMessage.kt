42a1d2fa54bbe3c9e0c7b6a4a93bc79c25f11a2d
mtorials <matti.bleckmann@live.com>
2020-06-16 18:54:43 +0200

    Renamed MessageContent to Content

--- src/main/kotlin/de/mtorials/dialphone/mevents/room/MRoomMessage.kt
+++ src/main/kotlin/de/mtorials/dialphone/mevents/room/MRoomMessage.kt
@@ -6,20 +6,19 @@ import com.fasterxml.jackson.annotation.JsonTypeName
 import de.mtorials.dialphone.ContentEventType
 import de.mtorials.dialphone.mevents.EventContent
 import de.mtorials.dialphone.mevents.MatrixEvent
-import de.mtorials.example.cutstomevents.PositionEvent
 
 @JsonTypeName("m.room.message")
 class MRoomMessage(
         override val sender: String,
         @JsonProperty("event_id")
         val id: String,
-        override val content: MessageContent
+        override val content: Content
 ) : MatrixEvent(sender, content) {
     @JsonIgnoreProperties(ignoreUnknown = true)
     @ContentEventType(MRoomMessage::class)
-    data class MessageContent(
+    data class Content(
         @JsonProperty("msgtype")
-        val msgType: String,
+        val msgType: String = "m.text",
         val body: String
     ) : EventContent
 }
