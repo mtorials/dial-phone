baacd9aeac802b7d9730216e3d2c6549c177f1f7
mtorials <matti.bleckmann@live.com>
2020-06-16 16:46:13 +0200

    some java docs

--- src/main/kotlin/de/mtorials/dial/listener/MatrixEventAdapter.kt
+++ src/main/kotlin/de/mtorials/dial/listener/MatrixEventAdapter.kt
@@ -5,10 +5,23 @@ import de.mtorials.dial.entityfutures.RoomFuture
 import de.mtorials.dial.mevents.MatrixEvent
 import kotlin.reflect.KClass
 
-abstract class MatrixEventAdapter<T : MatrixEvent>(private val clazz : KClass<T>) : Listener {
 
-    abstract fun onMatrixEvent(event: T, roomFuture: RoomFuture)
+/**
+ *
+ * Implement this class to listen specific custom events
+ *
+ * @param T The type of event you want to listen for
+ * @param type The class of the event type you want to listen for
+ *
+ */
+abstract class MatrixEventAdapter<T : MatrixEvent>(private val type : KClass<T>) : Listener {
     override fun onRoomEvent(event: MatrixEvent, roomId: String, phone: DialPhone) {
-        if (clazz.isInstance(event)) onMatrixEvent(event as T, RoomFuture(roomId, phone))
+        if (type.isInstance(event)) onMatrixEvent(event as T, RoomFuture(roomId, phone))
     }
+
+    /**
+     * @param event The event you listened for
+     * @param roomFuture The RoomFuture of the room the event occurred in
+     */
+    abstract fun onMatrixEvent(event: T, roomFuture: RoomFuture)
 }
