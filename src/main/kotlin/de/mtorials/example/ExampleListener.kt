77bbdba31f925ba95a6580b8e62236fcc1dd2c33
mtorials <matti.bleckmann@live.com>
2020-06-18 20:39:58 +0200

    Added Listener

--- src/main/kotlin/de/mtorials/example/ExampleListener.kt
+++ src/main/kotlin/de/mtorials/example/ExampleListener.kt
@@ -3,14 +3,13 @@ package de.mtorials.example
 import de.mtorials.dialphone.events.MessageReceivedEvent
 import de.mtorials.dialphone.events.answer
 import de.mtorials.dialphone.listener.ListenerAdapter
+import de.mtorials.dialphone.rename
 import de.mtorials.example.cutstomevents.PositionEvent
 
 class ExampleListener: ListenerAdapter() {
     override suspend fun onRoomMessageReceive(event: MessageReceivedEvent) {
         // Don't answer to your own messages
         if (event.senderId == event.phone.ownId) return
-        println("user ${event.senderId} send ${event.content.body}")
-        event answer "Hi"
-        //event.roomFuture.sendMessageEvent(PositionEvent.Content(1, 3, 5))
+        event.roomFuture rename "[Demo] ${event.content.body}"
     }
 }
--- src/main/kotlin/de/mtorials/example/MessageEventListener.kt
+++ src/main/kotlin/de/mtorials/example/MessageEventListener.kt
@@ -3,6 +3,7 @@ package de.mtorials.example
 import de.mtorials.dialphone.entities.entityfutures.RoomFutureImpl
 import de.mtorials.dialphone.listener.MatrixEventAdapter
 import de.mtorials.dialphone.mevents.roommessage.MatrixMessageEvent
+import de.mtorials.dialphone.rename
 
 class MessageEventListener() : MatrixEventAdapter<MatrixMessageEvent>(MatrixMessageEvent::class) {
     override fun onMatrixEvent(event: MatrixMessageEvent, roomFuture: RoomFutureImpl) {
