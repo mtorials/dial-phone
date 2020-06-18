b56569eb7e7df5230a095397e59d3be07997e5fb
mtorials <matti.bleckmann@live.com>
2020-06-18 16:31:02 +0200

    Added extension functions

--- src/main/kotlin/de/mtorials/dialphone/RoomExtensions.kt
+++ src/main/kotlin/de/mtorials/dialphone/RoomExtensions.kt
@@ -3,10 +3,13 @@ package de.mtorials.dialphone
 import de.mtorials.dialphone.entities.actions.RoomActions
 import de.mtorials.dialphone.entities.entityfutures.MessageFuture
 import de.mtorials.dialphone.mevents.roommessage.MRoomMessage
+import de.mtorials.dialphone.mevents.roomstate.MRoomName
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 infix fun RoomActions.write(message: String) = GlobalScope.launch { this@write.sendTextMessage(message) }
+infix fun RoomActions.rename(name: String) = setName(name)
+
 suspend fun RoomActions.sendAndGet(message: String) =
     MessageFuture(this@sendAndGet.sendTextMessage(message), this@sendAndGet.phone)
 
@@ -16,3 +19,7 @@ suspend fun RoomActions.sendTextMessage(content: String) : String = this.sendMes
         body = content
     )
 )
+
+fun RoomActions.setName(name: String) = GlobalScope.launch {
+    this@setName.sendStateEvent(MRoomName.Content(name = name))
+}
