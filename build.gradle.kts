855bb9b99df23b146779f7d2cb0aa78fe0851808
mtorials <matti.bleckmann@live.com>
2020-06-23 16:04:45 +0200

    using gradle kotlin dsl now

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+    id("org.jetbrains.dokka") version "0.10.0"
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.+")
+    implementation("com.github.kittinunf.fuel:fuel:2.2.2")
+    implementation("com.github.kittinunf.fuel:fuel-coroutines:2.2.2")
+    implementation("org.requirementsascode:moonwlker:0.0.6")
+    testImplementation("junit:junit:4.12")
+}
+
+tasks.dokka {
+    outputFormat = "html"
+    outputDirectory = "$buildDir/javadoc"
+}
