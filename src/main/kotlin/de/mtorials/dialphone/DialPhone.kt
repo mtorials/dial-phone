b58c897f8277b1d54c3880afe3d3aa866e875f56
mtorials <matti.bleckmann@live.com>
2020-06-24 11:38:13 +0200

    documented DialPhone.kt

--- src/main/kotlin/de/mtorials/dialphone/DialPhone.kt
+++ src/main/kotlin/de/mtorials/dialphone/DialPhone.kt
@@ -7,17 +7,58 @@ import kotlinx.coroutines.Job
 
 interface DialPhone {
 
+    /**
+     * The token for the matrix account
+     */
     val token: String
+
+    /**
+     * The id of the account you logged in with
+     */
     val ownId: String
+
+    /**
+     * The url of the homeserver
+     */
     val homeServerUrl: String
+
+    /**
+     * The command prefix you specified optionally
+     */
     val commandPrefix: String
+
+    /**
+     * The object the api uses to make low level requests
+     */
     val requestObject: APIRequests
 
+    /**
+     * Used to synchronize with the homeserver.
+     * Call this function to receive events
+     * @return Return an infinite lasting job
+     */
     fun sync() : Job
+
+    /**
+     * Adds a listener
+     */
     fun addListener(listener: Listener)
 
+    /**
+     * @return Return the entity futures of all rooms the user has joined
+     */
     suspend fun getJoinedRoomFutures() : List<RoomFuture>
+
+    /**
+     * @param id The id of the user you want to get
+     * @return Return null if not found, otherwise the user object
+     */
     suspend fun getUserById(id: String) : User?
+
+    /**
+     * @param id The id of the room you want to get
+     * @return Returns the room future. If not found: null
+     */
     suspend fun getJoinedRoomFutureById(id: String) : RoomFuture?
 
     companion object {
