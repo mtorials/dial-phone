da7c43b1dcfe41afe4016baada4faa7a30baf7c5
mtorials <git@mtorials.de>
2020-08-08 17:31:46 +0200

    Ouh man I dont know....

--- src/main/kotlin/de/mtorials/dialphone/mevents/MatrixRoomEvent.kt
+++
@@ -1,6 +0,0 @@
-package de.mtorials.dialphone.mevents
-
-/**
- * A matrix event that relates to a room
- */
-interface MatrixRoomEvent : MatrixEvent
--- src/main/kotlin/de/mtorials/dialphone/mevents/roomstate/StateEventContent.kt
+++
@@ -1,5 +0,0 @@
-package de.mtorials.dialphone.mevents.roomstate
-
-import de.mtorials.dialphone.mevents.EventContent
-
-interface StateEventContent : EventContent
