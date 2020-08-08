8b78201aa35a4b12fbe1bdfebddf2b41725f3727
mtorials <git@mtorials.de>
2020-08-08 17:31:46 +0200

    Ouh man I dont know....

--- src/main/kotlin/de/mtorials/dialphone/mevents/EventContent.kt
+++
@@ -1,6 +0,0 @@
-package de.mtorials.dialphone.mevents
-
-/**
- * Used to mark the type of a content property of a MatrixEvent
- */
-interface EventContent
--- src/main/kotlin/de/mtorials/dialphone/mevents/roommessage/MessageEventContent.kt
+++
@@ -1,8 +0,0 @@
-package de.mtorials.dialphone.mevents.roommessage
-
-import de.mtorials.dialphone.mevents.EventContent
-
-/**
- * Represents a matrix event of type room state event
- */
-interface MessageEventContent : EventContent
