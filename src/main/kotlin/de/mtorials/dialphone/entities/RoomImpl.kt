55a8c3f2655f5441e17a102fa338a7c1ae26522f
mtorials <git@mtorials.de>
2020-12-13 12:04:17 +0100

    little fix

--- src/main/kotlin/de/mtorials/dialphone/entities/RoomImpl.kt
+++ src/main/kotlin/de/mtorials/dialphone/entities/RoomImpl.kt
@@ -4,7 +4,6 @@ import de.mtorials.dialphone.entities.entityfutures.RoomFutureImpl
 import net.micromes.makocommons.enums.JoinRule
 import net.micromes.makocommons.enums.Membership
 import net.micromes.makocommons.mevents.roommessage.MessageEventContent
-import de.mtorials.dialphone.mevents.roomstate.*
 import net.micromes.makocommons.mevents.roomstate.*
 
 class RoomImpl(
