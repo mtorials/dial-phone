e702143a687176c73b6ede78215a074ba47edcbf
mtorials <git@mtorials.de>
2020-12-30 23:39:53 +0100

    Profile simplified

--- src/commonMain/kotlin/de/mtorials/dialphone/Profile.kt
+++ src/commonMain/kotlin/de/mtorials/dialphone/Profile.kt
@@ -1,14 +1,17 @@
 package de.mtorials.dialphone
 
 class Profile(
-    private val phone: DialPhoneImpl,
-    val id: String
+    private val phone: DialPhoneImpl
 ) {
-    suspend fun receiveDisplayName() : String {
-        return phone.requestObject.getDisplayName(phone.ownId).displayName
+    suspend fun receiveDisplayName() : String? {
+        return phone.requestObject.getMe().displayName
     }
 
     suspend fun setDisplayName(displayName: String) {
-        phone.requestObject.setDisplayName(id, displayName)
+        phone.requestObject.setDisplayName(receiveID(), displayName)
+    }
+
+    suspend fun receiveID(): String {
+        return phone.requestObject.getMe().id
     }
 }
