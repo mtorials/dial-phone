f41b5d2089d91392e229f9e000b84630ffa0b672
mtorials <git@mtorials.de>
2020-12-30 22:48:20 +0100

    APIRequests.kt now multiplatform

--- src/commonMain/kotlin/de/mtorials/dialphone/APIRequests.kt
+++ src/commonMain/kotlin/de/mtorials/dialphone/APIRequests.kt
@@ -1,108 +1,116 @@
 package de.mtorials.dialphone
 
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.annotation.JsonTypeName
-import com.fasterxml.jackson.module.kotlin.MissingKotlinParameterException
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
-import de.mtorials.dialphone.exceptions.APIException
 import net.mt32.makocommons.mevents.roomstate.MatrixStateEvent
 import de.mtorials.dialphone.responses.*
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.util.date.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.InternalSerializationApi
+import kotlinx.serialization.serializer
+import net.mt32.makocommons.EventSerialization
 import net.mt32.makocommons.mevents.EventContent
 import net.mt32.makocommons.mevents.MatrixEvent
-import org.http4k.client.OkHttp
-import org.http4k.core.Method
-import java.net.URLEncoder
 import kotlin.random.Random
 import kotlin.reflect.KClass
+import kotlin.reflect.typeOf
 
 class APIRequests(
     val phone: DialPhone,
     subTypes: Array<KClass<out MatrixEvent>>
 ) {
-    private val mapper = jacksonObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL)
-    private val random = Random(System.currentTimeMillis().toInt() * 2834)
 
-    private val client = OkHttp()
+    private val random = Random(getTimeMillis().toInt() * 2834)
 
-    init { subTypes.forEach { mapper.registerSubtypes(it.java) } }
+    private val client = HttpClient {
+        install(JsonFeature) {
+            serializer = KotlinxSerializer(EventSerialization.matrixFormat)
+        }
+    }
 
-    suspend fun discoverRooms() : RoomDiscovery = request(Method.GET, "publicRooms")
-    suspend fun getJoinedRooms() : JoinedRooms = request(Method.GET, "joined_rooms")
-    suspend fun getMe() : UserResponse = request(Method.GET, "account/whoami")
+    init { subTypes.forEach { TODO("Subtypes") } }
+
+    suspend fun discoverRooms() : RoomDiscovery = request(HttpMethod.Get, "publicRooms")
+    suspend fun getJoinedRooms() : JoinedRooms = request(HttpMethod.Get, "joined_rooms")
+    suspend fun getMe() : UserResponse = request(HttpMethod.Get, "account/whoami")
     suspend fun getRoomsState(id: String) : Array<MatrixStateEvent> =
-        request<Array<MatrixEvent>>(Method.GET, "rooms/${id}/state")
+        request<Array<MatrixEvent>>(HttpMethod.Get, "rooms/${id}/state")
             .filterIsInstance<MatrixStateEvent>().toTypedArray()
-    suspend fun getUserById(id: String) : UserWithoutIDResponse? = request(Method.GET, "profile/${id}")
+    suspend fun getUserById(id: String) : UserWithoutIDResponse? = request(HttpMethod.Get, "profile/${id}")
     suspend fun getEventByIdAndRoomId(id: String, roomId: String) : MatrixEvent =
-        request(Method.GET, "rooms/${encode(roomId)}/event/${encode(id)}")
+        request(HttpMethod.Get, "rooms/${encode(roomId)}/event/${encode(id)}")
     suspend fun joinRoomWithId(id: String) : RoomResponse =
-        request(Method.POST, "rooms/${encode(id)}/join")
+        request(HttpMethod.Post, "rooms/${encode(id)}/join")
     suspend fun getRoomIdForAlias(alias: String) : AliasExchangeResponse =
-        request(Method.GET, "directory/room/${encode(alias)}")
+        request(HttpMethod.Get, "directory/room/${encode(alias)}")
 
     // Events
+    @ExperimentalSerializationApi
+    @InternalSerializationApi
     suspend fun sendMessageEvent(
         eventType: KClass<out MatrixEvent>,
         content: EventContent,
         roomID: String
     ) : String {
-        val typename = eventType.annotations.filterIsInstance<JsonTypeName>()[0].value
+        val typename = eventType.serializer().descriptor.serialName
         return request<EventResponse>(
-            method = Method.PUT,
+            httpMethod = HttpMethod.Put,
             path = "rooms/${encode(roomID)}/send/${typename}/${random.nextInt()}",
-            body = content
+            bodyValue = content
         ).id
     }
+
+    @ExperimentalSerializationApi
+    @InternalSerializationApi
     suspend fun sendStateEvent(
         eventType: KClass<out MatrixEvent>,
         content: EventContent,
         roomID: String,
         stateKey: String
     ) : String {
-        val typename = eventType.annotations.filterIsInstance<JsonTypeName>()[0].value
+        val typename = eventType.serializer().descriptor.serialName
         return request<EventResponse>(
-            method = Method.PUT,
+            httpMethod = HttpMethod.Put,
             path = "rooms/${encode(roomID)}/state/${typename}/${stateKey}",
-            body = content
+            bodyValue = content
         ).id
     }
     suspend fun redactEventWithIdInRoom(roomId: String, id: String, reason: String? = null) : EventResponse =
-        request(method = Method.PUT, path = "rooms/${encode(roomId)}/redact/${encode(id)}/${random.nextInt()}", body = Reason(reason))
+        request(httpMethod = HttpMethod.Put, path = "rooms/${encode(roomId)}/redact/${encode(id)}/${random.nextInt()}", bodyValue = Reason(reason))
 
     //Profile
     suspend fun getDisplayName(userId: String) : DisplayNameResponse =
-        request(Method.GET, "profile/${encode(userId)}/displayname")
+        request(HttpMethod.Get, "profile/${encode(userId)}/displayname")
     suspend fun setDisplayName(userId: String, displayName: String) : EmptyResponse =
         request(
-            method = Method.PUT,
+            httpMethod = HttpMethod.Put,
             path = "profile/${encode(userId)}/displayname",
-            body = object {
+            bodyValue = object {
                 val displayname: String = displayName
             }
         )
  
     private suspend inline fun <reified T> request(
-        method: Method,
+        httpMethod: HttpMethod,
         path: String,
         vararg parameters: Pair<String, String> = arrayOf(),
-        body: Any? = null
+        bodyValue: Any? = null
     ) : T {
         val newPath = phone.homeserverUrl + DialPhone.MATRIX_PATH + path
-        var request = org.http4k.core.Request(method, newPath)
-            .header("Authorization", "Bearer ${phone.token}")
-            .header("Content-Type", "application/json")
-        for (pair in parameters) {
-            request = request.query(pair.first, pair.second)
-        }
-        if (body != null) request = request.body(mapper.writeValueAsString(body))
-        val resString = client(request).bodyString()
-        try {
-            return jacksonObjectMapper().readValue(resString)
-        } catch(e: MissingKotlinParameterException) {
-            throw jacksonObjectMapper().readValue<APIException>(resString)
+        return client.request {
+            url(newPath)
+            method = httpMethod
+            header("Authorization", "Bearer ${phone.token}")
+            header("Content-Type", "application/json")
+            parameters.forEach { parameter(it.first, it.second) }
+            if (bodyValue != null) body = bodyValue
         }
     }
 
-    private fun encode(input: String) = URLEncoder.encode(input, "utf-8")
+    // Useless hopefully
+    private fun encode(input: String) = input
 }
