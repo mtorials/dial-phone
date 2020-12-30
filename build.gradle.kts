f55d6f989fc7fe4f28483a7169868bc7fc63867f
mtorials <git@mtorials.de>
2020-12-30 16:44:51 +0100

    build file to mutliplatform

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("multiplatform") version "1.4.21"
-    //kotlin("plugin.serialization") version "1.4.10"
+    kotlin("plugin.serialization") version "1.4.10"
 }
 
 group = "de.mtorials"
@@ -18,19 +18,14 @@ kotlin {
 
     sourceSets {
         val commonMain by getting {
-
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
+                implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.+")
+                implementation("org.http4k:http4k-core:3.254.0")
+                implementation("org.http4k:http4k-client-okhttp:3.254.0")
+            }
         }
     }
-
-    dependencies {
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.+")
-        implementation(group = "org.http4k", name = "http4k-core", version = "3.254.0")
-        implementation(group = "org.http4k", name = "http4k-client-okhttp", version = "3.254.0")
-
-        testCompileOnly("io.kotlintest:kotlintest-core:3.0.2")
-        testCompileOnly("io.kotlintest:kotlintest-assertions:3.0.2")
-        testCompileOnly("io.kotlintest:kotlintest-runner-junit5:3.0.2")
-    }
 }
+
 val artifactID = "dial-phone"
