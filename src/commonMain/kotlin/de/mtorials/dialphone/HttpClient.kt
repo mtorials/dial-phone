325a804bf6b5ebf7e41095e04499087facbfa6c1
mtorials <git@mtorials.de>
2020-12-30 23:08:03 +0100

    Synchronizer.kt is now multiplatform

---
+++ src/commonMain/kotlin/de/mtorials/dialphone/HttpClient.kt
@@ -0,0 +1,14 @@
+package de.mtorials.dialphone
+
+import io.ktor.client.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import net.mt32.makocommons.EventSerialization
+
+object HttpClient {
+    val client = HttpClient {
+        install(JsonFeature) {
+            serializer = KotlinxSerializer(EventSerialization.matrixFormat)
+        }
+    }
+}
