78c8ab8e6a80602231fd83cf9b5249a23fde8d51
mtorials <git@mtorials.de>
2021-01-01 18:04:17 +0100

    nullables now default null in events

--- src/commonMain/kotlin/de/mtorials/dialphone/responses/ReasonResponse.kt
+++ src/commonMain/kotlin/de/mtorials/dialphone/responses/ReasonResponse.kt
@@ -4,5 +4,5 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ReasonResponse(
-    val reason: String?
+    val reason: String? = null
 )
--- src/commonMain/kotlin/net/mt32/makocommons/mevents/roomstate/MRoomMember.kt
+++ src/commonMain/kotlin/net/mt32/makocommons/mevents/roomstate/MRoomMember.kt
@@ -10,19 +10,19 @@ import net.mt32.makocommons.mevents.ContentEventType
 class MRoomMember(
     override val sender: String,
     @SerialName("event_id")
-    override val id: String?,
+    override val id: String? = null,
     override val content: Content,
     @SerialName("state_key")
     override val stateKey: String,
     @SerialName("prev_content")
-    override val prevContent: Content?
+    override val prevContent: Content? = null
 ) : MatrixStateEvent {
     @ContentEventType(MRoomMember::class)
     @Serializable
     data class Content(
         val membership: Membership,
         @SerialName("avatar_url")
-        val avatarURL: String?,
+        val avatarURL: String? = null,
         @SerialName("displayname")
         val displayName: String?
     ) : StateEventContent
