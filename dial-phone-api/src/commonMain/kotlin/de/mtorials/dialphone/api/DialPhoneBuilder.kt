6cfbe320aec705bf346d5cbadd44207a6d6ee1a9
mtorials <git@mtorials.de>
2022-01-19 00:06:50 +0100

    docs and refactoring/renaming

--- dial-phone-api/src/commonMain/kotlin/de/mtorials/dialphone/api/DialPhoneBuilder.kt
+++ dial-phone-api/src/commonMain/kotlin/de/mtorials/dialphone/api/DialPhoneBuilder.kt
@@ -7,21 +7,27 @@ import kotlinx.serialization.modules.SerializersModule
 interface DialPhoneBuilder {
     /**
      * Authenticate as guest user
-     * Can be used as an alternative to isUser() and withToken()
+     * Can be used as an alternative to
+     * @see asUser
+     * @see withToken
      */
-    fun isGuest()
+    fun asGuest()
 
     /**
      * Authenticate as user with credentials
      * @param createUserIfNotRegistered creates a user in case the user os not already registered, default is false
-     * Can be used as an alternative to isGuest() and withToken()
+     * Can be used as an alternative to
+     * @see asGuest
+     * @see withToken
      */
-    fun isUser(username: String, password: String, createUserIfNotRegistered: Boolean = false)
+    fun asUser(username: String, password: String, createUserIfNotRegistered: Boolean = false)
 
     /**
      * Authenticate a user with access token
      * @param token The access token can be retrieved using a matrix client like element
-     * Can be used as an alternative to usGuest() and isUser()
+     * Can be used as an alternative to
+     * @see asUser
+     * @see asGuest
      */
     fun withToken(token: String)
 
