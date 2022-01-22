9ac0a0af688f2ec3a62d56ba82a91cd51130ef01
mtorials <git@mtorials.de>
2022-01-22 16:02:18 +0100

    implement key upload api

---
+++ dial-phone-api/src/commonMain/kotlin/de/mtorials/dialphone/api/responses/encryption/KeyUploadResponse.kt
@@ -0,0 +1,17 @@
+package de.mtorials.dialphone.api.responses.encryption
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+/**
+ * https://spec.matrix.org/v1.1/client-server-api/#key-management-api
+ */
+data class KeyUploadResponse(
+    /**
+     * For each key algorithm,
+     * the number of unclaimed one-time keys of that type currently held on the server for this device.
+     */
+    @SerialName("one_time_key_count")
+    val oneTimeKeyCount: Map<String, String>,
+)
