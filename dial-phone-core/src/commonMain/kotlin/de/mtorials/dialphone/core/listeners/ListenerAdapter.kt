1aab554a78f4d65375b8e7bcecc74d805a9f9dbe
mtorials <git@mtorials.de>
2022-01-26 11:40:06 +0100

    make ListenerAdapter not a listener

--- dial-phone-core/src/commonMain/kotlin/de/mtorials/dialphone/core/listeners/ListenerAdapter.kt
+++ dial-phone-core/src/commonMain/kotlin/de/mtorials/dialphone/core/listeners/ListenerAdapter.kt
@@ -1,16 +1,14 @@
 package de.mtorials.dialphone.core.listeners
 
-import de.mtorials.dialphone.api.listeners.GenericListener
-import de.mtorials.dialphone.core.DialPhone
 import de.mtorials.dialphone.core.dialevents.MessageReceivedEvent
 import de.mtorials.dialphone.core.dialevents.RoomInviteEvent
 
-interface ListenerAdapter : GenericListener<DialPhone> {
+interface ListenerAdapter {
     fun onRoomMessageReceived(block: suspend (MessageReceivedEvent) -> Unit)
     fun onRoomInvited(block: suspend (RoomInviteEvent) -> Unit)
 
     companion object {
-        operator fun invoke(receivePastEvents: Boolean = false, block: ListenerAdapter.() -> Unit) : ListenerAdapter {
+        operator fun invoke(receivePastEvents: Boolean = false, block: ListenerAdapter.() -> Unit) : ListenerAdapterImpl {
             val adapter = ListenerAdapterImpl(receivePastEvents)
             adapter.block()
             return adapter
