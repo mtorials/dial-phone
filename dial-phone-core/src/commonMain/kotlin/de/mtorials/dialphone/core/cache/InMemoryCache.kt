b1c7c21a846ccdc44d3404c3a44e14fb558cb2e8
mtorials <git@mtorials.de>
2022-02-12 12:37:53 +0100

    restructure cache

--- dial-phone-core/src/commonMain/kotlin/de/mtorials/dialphone/core/cache/InMemoryCache.kt
+++ dial-phone-core/src/commonMain/kotlin/de/mtorials/dialphone/core/cache/InMemoryCache.kt
@@ -1,10 +1,26 @@
 package de.mtorials.dialphone.core.cache
 
-import de.mtorials.dialphone.core.entities.User
-import de.mtorials.dialphone.core.entityfutures.RoomFuture
+import de.mtorials.dialphone.api.ids.RoomId
+import de.mtorials.dialphone.api.model.mevents.roomstate.MatrixStateEvent
 
 class InMemoryCache : PhoneCache {
-    override var joinedRooms: MutableCollection<RoomFuture> = mutableListOf()
-    override var invitedRooms: MutableCollection<InvitedRoomActions> = mutableListOf()
-    override val users: MutableMap<String, User> = mutableMapOf()
+    override val roomCache = object : RoomCache {
+
+        private val states: MutableMap<RoomId, MutableList<MatrixStateEvent>> = mutableMapOf()
+
+        override fun getRoomStateEvents(roomId: RoomId): List<MatrixStateEvent> = states[roomId] ?: emptyList()
+
+        // TODO testing!
+        override fun insertRoomStateEvent(roomId: RoomId, event: MatrixStateEvent) {
+            states[roomId] = states[roomId]?.filter {
+                !(event::class.isInstance(it) && event.stateKey == it.stateKey)
+            }?.toMutableList() ?: mutableListOf()
+            states[roomId]?.add(event)
+        }
+
+        override var joinedRoomIds: Collection<RoomId> = listOf()
+        override var invitedRoomIds: Collection<RoomId> = listOf()
+        override var knockedRoomIds: Collection<RoomId> = listOf()
+        override var leftRoomIds: Collection<RoomId> = listOf()
+    }
 }
---
+++ dial-phone-core/src/commonMain/kotlin/de/mtorials/dialphone/core/cache/MessageCache.kt
@@ -0,0 +1,4 @@
+package de.mtorials.dialphone.core.cache
+
+interface MessageCache {
+}
