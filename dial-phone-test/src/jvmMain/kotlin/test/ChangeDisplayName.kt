97b91dc75e129c064c5464d55020173f8ef3ee5a
mtorials <git@mtorials.de>
2022-05-12 14:58:46 +0200

    small fixes, ci/cd small change

--- dial-phone-test/src/jvmMain/kotlin/test/ChangeDisplayName.kt
+++ dial-phone-test/src/jvmMain/kotlin/test/ChangeDisplayName.kt
@@ -1,8 +1,16 @@
 package test
 
+import de.mtorials.dialhone.bot.Command
+import de.mtorials.dialhone.bot.CommandListener
+import de.mtorials.dialphone.api.ids.RoomId
+import de.mtorials.dialphone.api.ids.UserId
 import de.mtorials.dialphone.api.model.enums.Membership
 import de.mtorials.dialphone.api.model.mevents.roomstate.MRoomMember
 import de.mtorials.dialphone.core.DialPhone
+import de.mtorials.dialphone.core.dialevents.answer
+import de.mtorials.dialphone.core.listeners.ListenerAdapter
+import de.mtorials.dialphone.core.sendTextMessage
+import de.mtorials.dialphone.encyption.useEncryption
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -17,20 +25,43 @@ const val MATRIX_SERVER = "https://matrix.mtorials.de"
 data class ChangeConfig(val token: String)
 val changeConfig : ChangeConfig = Json.decodeFromString(File("config.json").readText())
 
-    fun main() {
-        runBlocking {
-            val phone = DialPhone(MATRIX_SERVER) {
-                withToken(changeConfig.token)
-            }
+val ping = Command("ping", "ping the bot", "ping") {
+    this answer "pong!"
+}
+
+val changeName = Command("cname", "change my display name in this room", "cname <new_name>") {
+    if (it.isEmpty()) {
+        this answer "Ich brauch nen namen!"
+        return@Command
+    }
+    room.sendStateEvent(MRoomMember.Content(
+        Membership.JOIN,
+        null,
+        displayName = it[0],
+    ), "m.room.member")
+    this answer "Hat das geklappt?"
+}
+
+
+fun main() {
+    runBlocking {
+        val phone = DialPhone(MATRIX_SERVER) {
+            withToken(changeConfig.token)
+            addListeners(CommandListener("!", listOf(ping, changeName), ping))
+//                useEncryption()
+        }
 //            val job = phone.sync()
 //                delay(10000)
-            println("Hello")
+        println("Hello")
 //            delay(5000)
-            println(phone.getJoinedRooms().map { it.name })
-            phone.getJoinedRoomByName("temp Trium Machina")?.sendStateEvent(MRoomMember.Content(
-                Membership.JOIN,
-                null,
-                displayName = "mt",
-            ), "m.room.member")
-        }
+        println(phone.getJoinedRooms().map { it.name })
+        val trium = phone.getJoinedRoomById(RoomId("!ZxGwuWTrOPAJsqplVU:mtorials.de"))?: error("404")
+//            trium.sendStateEvent(MRoomMember.Content(
+//                Membership.JOIN,
+//                null,
+//                displayName = "mt",
+//            ), "m.room.member")
+        trium.sendTextMessage("Buh! probiert mal ein !ping und ein !cname!")
+        phone.sync().join()
     }
+}
