422be5c0a7175ab0f680efaee68874d3d73d56ad
mtorials <git@mtorials.de>
2022-05-13 12:39:42 +0200

    configs for tests

---
+++ dial-phone-test/src/jvmTest/kotlin/de/mtorials/dialphone/test/integration/Configs.kt
@@ -0,0 +1,9 @@
+package de.mtorials.dialphone.test.integration
+
+object Configs {
+    const val HOMESERVER_URL = "http://localhost:80"
+    const val TEST_USER = "helloasd"
+    const val TEST_PWD = "superduper"
+    const val USER2_USER = "user2"
+    const val USER2_PWD = "test"
+}
