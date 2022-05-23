926e04794ac8a52cbe93f1850b9022fd251c9bc8
mtorials <git@mtorials.de>
2022-05-23 15:33:15 +0200

    building matrix-rust-sdk

--- dial-phone-olm-machine/build.gradle.kts
+++ dial-phone-olm-machine/build.gradle.kts
@@ -1,3 +1,5 @@
+import de.undercouch.gradle.tasks.download.Download
+
 plugins {
     kotlin("multiplatform")
     kotlin("plugin.serialization")
@@ -27,4 +29,39 @@ kotlin {
             }
         }
     }
+}
+
+val sdkVersion = "0.5.1"
+
+val sdkRoot = projectDir.resolve("sdk/$sdkVersion")
+val zipFile = sdkRoot.resolve("sdk.zip")
+val buildDir = sdkRoot.resolve("build")
+
+val downloadSdk by tasks.registering(Download::class) {
+    src("https://github.com/matrix-org/matrix-rust-sdk/archive/refs/tags/matrix-sdk-base-$sdkVersion.zip")
+    dest(zipFile)
+}
+
+val unzipSdk by tasks.registering(Copy::class) {
+    from(zipTree(zipFile)) {
+        include("*/**")
+        eachFile {
+            relativePath = RelativePath(true, *relativePath.segments.drop(1).toTypedArray())
+        }
+    }
+    into(buildDir)
+    dependsOn(downloadSdk)
+}
+
+val buildSdk by tasks.registering(Exec::class) {
+    workingDir(buildDir)
+    commandLine("cargo", "build",  "--release", "-p",  "matrix-sdk-crypto-ffi")
+    dependsOn(unzipSdk)
+}
+
+val copySdk by tasks.registering(Copy::class) {
+    from(buildDir.resolve("target/release/libmatrix_crypto.so"))
+    into(projectDir.resolve("src/jvmMain/resources/"))
+    rename("libmatrix_crypto.so", "libuniffi_olm.so")
+    dependsOn(buildSdk)
 }
